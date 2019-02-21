82f43f653bea23d871cf2aaaa0e0ef430896b486
amitmegha <amit_1_man@yahoo.co.in>
2019-02-21 03:42:50 +0000

    Delete stage-publish.sh

--- stage-publish.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-jfrog rt c --url="$ARTIFACTORY_BASE_URL"/ --apikey="AKCp5btybYir2PJhM9aYursJEFvQvSGZPAoMvas7o5fnpAhruL5ouwnPCdBZXPCC4snqEE5cD"
-jfrog rt u "src/main" "$REPO_NAME"/main_"$CI_PIPELINE_ID" --recursive=false
